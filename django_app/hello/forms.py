2b40ab11dcc207e728a83e784c56d5f9bae25d70
Shunsuke Hioki <>
2019-05-03 02:33:52 +0900

    add create method

--- django_app/hello/forms.py
+++ django_app/hello/forms.py
@@ -1,5 +1,16 @@
 from django import forms
+from .models import Friend
+
+
+class FriendForm(forms.ModelForm):
+    class Meta:
+        model = Friend
+        fields = ['name', 'mail', 'gender', 'age', 'birthday']
 
 
 class HelloForm(forms.Form):
-    id = forms.IntegerField(label='ID')
+    name = forms.CharField(label='Name')
+    mail = forms.EmailField(label='Email')
+    gender = forms.BooleanField(label='Gender', required=False)
+    age = forms.IntegerField(label='Age')
+    birthday = forms.DateField(label='Birth')
