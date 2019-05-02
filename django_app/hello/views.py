2ec6a6e2d58064c30b62762db6bd0f3d66449309
Shunsuke Hioki <>
2019-05-02 20:15:54 +0900

    introduce template

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -5,4 +5,18 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'hello/index.html')
+    params = {
+        'title': 'Hello/Index',
+        'msg': 'これは，サンプルでつくったページです',
+        'goto': 'next'
+    }
+    return render(request, 'hello/index.html', params)
+
+
+def next(request):
+    params = {
+        'title': 'Hello/Next',
+        'msg': 'これは，もうひとつのページです',
+        'goto': 'index',
+    }
+    return render(request, 'hello/index.html', params)
