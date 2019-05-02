8a57f6183fb161debbbe78c5a645e78ec7e19a31
Shunsuke Hioki <>
2019-05-02 17:05:56 +0900

    get values from queryparameter

--- django_app/hello/views.py
+++ django_app/hello/views.py
@@ -3,5 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Hello Django!")
+
+def index(request, id, nickname):
+    result = 'you id:' + str(id) + ',name:"'+nickname+'".'
+    return HttpResponse(result)
